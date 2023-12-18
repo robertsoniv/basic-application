689ab8fa78218b926d10d11060291a587feb6e93
Robert Watt <rob.watt@sitecore.com>
2023-12-18 01:03:42 -0600

    Update configs

--- src/features/domains/ordercloud/constants.ts
+++ src/features/domains/ordercloud/constants.ts
@@ -6,8 +6,8 @@ const CLIENT_ID = import.meta.env.VITE_APP_ORDERCLOUD_CLIENT_ID;
 const SCOPE_STRING = import.meta.env.VITE_APP_ORDERCLOUD_SCOPE;
 const CUSTOM_SCOPE_STRING = import.meta.env.VITE_APP_ORDERCLOUD_SCOPE;
 
-const SCOPE:ApiRole[] = SCOPE_STRING.split(',');
-const CUSTOM_SCOPE:string[] = CUSTOM_SCOPE_STRING.split(',');
+const SCOPE:ApiRole[] = SCOPE_STRING?.split(',');
+const CUSTOM_SCOPE:string[] = CUSTOM_SCOPE_STRING?.split(',');
 
 //Anonymous auth configuration
 const ALLOW_ANONYMOUS = import.meta.env.VITE_APP_ORDERCLOUD_ALLOW_ANONYMOUS === "true";
