cb9cb9d265295dbdc515e94cf88aa0e6c4825158
Robert Watt <rob.watt@sitecore.com>
2023-12-18 13:40:51 -0600

    revert vite.config to basic implementation

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base = process.env.VITE_APP_CONFIG_BASE;
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_APP_CONFIG_BASE?.length > 1 ? `${process.env.VITE_APP_CONFIG_BASE}/` : undefined,
+  base,
   plugins: [react()],
 })
