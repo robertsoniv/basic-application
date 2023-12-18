773e4769f52d4d9235a85c324c6870d760d6668a
Robert Watt <rob.watt@sitecore.com>
2023-12-18 15:30:16 -0600

    try manually defining in vite.config

--- src/features/domains/ordercloud/constants.ts
+++ src/features/domains/ordercloud/constants.ts
@@ -1,16 +1,16 @@
 import { ApiRole } from "ordercloud-javascript-sdk";
 
 //Basic auth configuration
-const BASE_API_URL = import.meta.env.VITE_APP_ORDERCLOUD_BASE_API_URL || "https://api.ordercloud.io/v1";
-const CLIENT_ID = import.meta.env.VITE_APP_ORDERCLOUD_CLIENT_ID;
-const SCOPE_STRING = import.meta.env.VITE_APP_ORDERCLOUD_SCOPE;
-const CUSTOM_SCOPE_STRING = import.meta.env.VITE_APP_ORDERCLOUD_SCOPE;
+const BASE_API_URL = process.env.VITE_APP_ORDERCLOUD_BASE_API_URL || "https://api.ordercloud.io/v1";
+const CLIENT_ID = process.env.VITE_APP_ORDERCLOUD_CLIENT_ID;
+const SCOPE_STRING = process.env.VITE_APP_ORDERCLOUD_SCOPE;
+const CUSTOM_SCOPE_STRING = process.env.VITE_APP_ORDERCLOUD_CUSTOM_SCOPE;
 
-const SCOPE:ApiRole[] = SCOPE_STRING.split(',') as ApiRole[];
+const SCOPE:ApiRole[] = SCOPE_STRING?.split(',') as ApiRole[] || [];
 const CUSTOM_SCOPE:string[] = CUSTOM_SCOPE_STRING?.split(',') || [];
 
 //Anonymous auth configuration
-const ALLOW_ANONYMOUS_STRING = import.meta.env.VITE_APP_ORDERCLOUD_ALLOW_ANONYMOUS;
+const ALLOW_ANONYMOUS_STRING = process.env.VITE_APP_ORDERCLOUD_ALLOW_ANONYMOUS;
 const ALLOW_ANONYMOUS = ALLOW_ANONYMOUS_STRING ? ALLOW_ANONYMOUS_STRING === "true" : false;
 
 console.log('consts', {
--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,12 @@ export default defineConfig(({ mode }) => {
     return {
       base: env.VITE_APP_CONFIG_BASE,
       plugins: [react()],
+      define: {
+        'process.env.VITE_APP_CONFIG_BASE': env.VITE_APP_CONFIG_BASE,
+        'process.env.VITE_APP_ORDERCLOUD_CLIENT_ID': env.VITE_APP_ORDERCLOUD_CLIENT_ID,
+        'process.env.VITE_APP_ORDERCLOUD_SCOPE': env.VITE_APP_ORDERCLOUD_SCOPE,
+        'process.env.VITE_APP_ORDERCLOUD_CUSTOM_SCOPE': env.VITE_APP_ORDERCLOUD_CUSTOM_SCOPE,
+        'process.env.VITE_APP_ORDERCLOUD_ALLOW_ANONYMOUS': env.VITE_APP_ORDERCLOUD_ALLOW_ANONYMOUS,
+      }
     };
 });
