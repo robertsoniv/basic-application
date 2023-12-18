a6e8f01dbabed09201fb5936452e92d9e8f5de32
Robert Watt <rob.watt@sitecore.com>
2023-12-18 13:57:08 -0600

    update vite.config.ts to use loadEnv

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'
 
-const base = process.env.VITE_APP_CONFIG_BASE;
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base,
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+      base: env.VITE_APP_CONFIG_BASE,
+      plugins: [react()],
+    };
+});
