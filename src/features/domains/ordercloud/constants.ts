b7ef1906fd28b935aa7c65931ebb36cce0dbc9a9
Robert Watt <rob.watt@sitecore.com>
2023-12-18 16:43:04 -0600

    clean up code

--- src/features/domains/ordercloud/constants.ts
+++ src/features/domains/ordercloud/constants.ts
@@ -1,26 +1,16 @@
 import { ApiRole } from "ordercloud-javascript-sdk";
 
 //Basic auth configuration
-const CONFIG_BASE = import.meta.env.VITE_APP_CONFIG_BASE;
 const BASE_API_URL = import.meta.env.VITE_APP_ORDERCLOUD_BASE_API_URL || "https://api.ordercloud.io/v1";
-const CLIENT_ID = import.meta.env.VITE_APP_ORDERCLOUD_CLIENT_ID || "";
-const SCOPE_STRING = import.meta.env.VITE_APP_ORDERCLOUD_SCOPE || "";
+const CLIENT_ID = import.meta.env.VITE_APP_ORDERCLOUD_CLIENT_ID;
+const SCOPE_STRING = import.meta.env.VITE_APP_ORDERCLOUD_SCOPE;
 const CUSTOM_SCOPE_STRING = import.meta.env.VITE_APP_ORDERCLOUD_CUSTOM_SCOPE;
 
-const SCOPE:ApiRole[] = SCOPE_STRING.split(',') as ApiRole[];
-const CUSTOM_SCOPE:string[] = CUSTOM_SCOPE_STRING?.split(',') || [];
+const SCOPE:ApiRole[] = SCOPE_STRING?.length ? SCOPE_STRING.split(',') as ApiRole[] : [];
+const CUSTOM_SCOPE:string[] = CUSTOM_SCOPE_STRING?.length ? CUSTOM_SCOPE_STRING.split(',') : [];
 
 //Anonymous auth configuration
 const ALLOW_ANONYMOUS = import.meta.env.VITE_APP_ORDERCLOUD_ALLOW_ANONYMOUS || false;
-
-console.log('consts', {
-    CONFIG_BASE,
-    BASE_API_URL,
-    CLIENT_ID,
-    SCOPE,
-    CUSTOM_SCOPE,
-    ALLOW_ANONYMOUS,
-}, )
 
 export {
     BASE_API_URL,
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,8 +4,8 @@ interface ImportMetaEnv {
   readonly VITE_APP_CONFIG_BASE?: string;
   readonly VITE_APP_ORDERCLOUD_BASE_API_URL?: string;
   readonly VITE_APP_ORDERCLOUD_CLIENT_ID: string;
-  readonly VITE_APP_ORDERCLOUD_SCOPE: string;
   readonly VITE_APP_ORDERCLOUD_SCOPE?: string;
+  readonly VITE_APP_ORDERCLOUD_CUSTOM_SCOPE?: string;
   readonly VITE_APP_ORDERCLOUD_ALLOW_ANONYMOUS?: boolean;
   // more env variables...
 }
